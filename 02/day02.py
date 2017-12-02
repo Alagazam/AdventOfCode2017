bb528777d8972ed983291be78a3c63520950c7e5
David Darj <zid@alagazam.net>
2017-12-02 21:25:16 +0100

    Day 2 part 2

--- 02/day02.py
+++ 02/day02.py
@@ -6,9 +6,29 @@ class checksum:
             sum += max(numlist) - min(numlist)
         return sum
 
+    def calculate2(input):
+        sum=0
+        for row in input:
+            numlist = [int(x) for x in row.split("\t")]
+            div = 0
+            for i in range(0,len(numlist)-1):
+                for j in range(i+1,len(numlist)):
+                    big = max(numlist[i],numlist[j])
+                    small = min(numlist[i],numlist[j])
+                    if (big/small == big//small):
+                        div = big//small
+                        break
+                if (div != 0):
+                    break
+
+            sum += div
+
+        return sum
+
+
 if __name__ == '__main__':
     file = open("input.txt", "r")
     input = file.read().splitlines()
     file.close()
     print("checksum1: " + str(checksum.calculate(input)))
-    #print("Captcha2: " + str(captcha.calculate2(input)))
+    print("checksum2: " + str(checksum.calculate2(input)))
--- 02/test02.py
+++ 02/test02.py
@@ -3,10 +3,13 @@ from day02 import checksum
 
 class Test_test02(unittest.TestCase):
 
-    testdata = ["5\t1\t9\t5", "7\t5\t3", "2\t4\t6\t8"]
-
     def test_checksum(self):
-        self.assertEqual(18, checksum.calculate(self.testdata))
+        testdata1 = ["5\t1\t9\t5", "7\t5\t3", "2\t4\t6\t8"]
+        self.assertEqual(18, checksum.calculate(testdata1))
+
+    def test_checksum2(self):
+        testdata2 = ["5\t9\t2\t8", "9\t4\t7\t3", "3\t8\t6\t5"]
+        self.assertEqual(9, checksum.calculate2(testdata2))
 
 if __name__ == '__main__':
     unittest.main()
