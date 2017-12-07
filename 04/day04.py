e8b740865e59cfc8a2921cd1cb9135eb82940636
David Darj <zid@alagazam.net>
2017-12-07 22:52:37 +0100

    Day 4 step 2

--- 04/day04.py
+++ 04/day04.py
@@ -2,20 +2,29 @@
 
 class PassPhraseCheck:
     @staticmethod
-    def is_pass_phrase_valid(pass_phrase):
+    def is_pass_phrase_valid1(pass_phrase):
         wordlist = [word for word in pass_phrase.split(" ")]
         wordset = set(wordlist)
         return len(wordlist) == len(wordset)
 
     @staticmethod
+    def is_pass_phrase_valid2(pass_phrase):
+        wordlist = [''.join(sorted(word)) for word in pass_phrase.split(" ")]
+        print(wordlist)
+        wordset = set(wordlist)
+        return len(wordlist) == len(wordset)
+
+    @staticmethod
     def check_pass_phrases(pass_phrases):
-        valid = 0
+        valid1 = 0
+        valid2 = 0
         for row in pass_phrases:
             print(row)
-            if PassPhraseCheck.is_pass_phrase_valid(row):
-                print("   VALID")
-                valid += 1
-        return valid
+            if PassPhraseCheck.is_pass_phrase_valid1(row):
+                valid1 += 1
+            if PassPhraseCheck.is_pass_phrase_valid2(row):
+                valid2 += 1
+        return valid1, valid2
 
 if __name__ == '__main__':
     FILE = open("input.txt", "r")
--- 04/test04.py
+++ 04/test04.py
@@ -4,14 +4,25 @@ import unittest
 from day04 import PassPhraseCheck
 
 class Test04(unittest.TestCase):
-    def test_check_pass_valid(self):
-        self.assertTrue(PassPhraseCheck.is_pass_phrase_valid("aa bb cc dd ee"))
-        self.assertFalse(PassPhraseCheck.is_pass_phrase_valid("aa bb cc dd aa"))
-        self.assertTrue(PassPhraseCheck.is_pass_phrase_valid("aa bb cc dd aaa"))
+    def test_check_pass_valid1(self):
+        self.assertTrue(PassPhraseCheck.is_pass_phrase_valid1("aa bb cc dd ee"))
+        self.assertFalse(PassPhraseCheck.is_pass_phrase_valid1("aa bb cc dd aa"))
+        self.assertTrue(PassPhraseCheck.is_pass_phrase_valid1("aa bb cc dd aaa"))
+
+    def test_check_pass_valid2(self):
+        self.assertTrue(PassPhraseCheck.is_pass_phrase_valid2("aa bb cc dd ee"))
+        self.assertFalse(PassPhraseCheck.is_pass_phrase_valid2("aa bb cc dd aa"))
+        self.assertTrue(PassPhraseCheck.is_pass_phrase_valid2("aa bb cc dd aaa"))
+
+        self.assertTrue(PassPhraseCheck.is_pass_phrase_valid2("abcde fghij"))
+        self.assertFalse(PassPhraseCheck.is_pass_phrase_valid2("abcde xyz ecdab"))
+        self.assertTrue(PassPhraseCheck.is_pass_phrase_valid2("a ab abc abd abf abj"))
+        self.assertTrue(PassPhraseCheck.is_pass_phrase_valid2("iiii oiii ooii oooi oooo"))
+        self.assertFalse(PassPhraseCheck.is_pass_phrase_valid2("oiii ioii iioi iiio"))
 
     def test_check_pass_phrases(self):
-        testdata = ["aa bb cc dd ee", "aa bb cc dd aa", "aa bb cc dd aaa"]
-        self.assertEqual(2, PassPhraseCheck.check_pass_phrases(testdata))
+        testdata = ["aa bb cc dd ee", "aa bb cc dd aa", "aa bb cc dd aaa", "oiii ioii iioi iiio"]
+        self.assertEqual((3, 2), PassPhraseCheck.check_pass_phrases(testdata))
 
 if __name__ == '__main__':
     unittest.main()
