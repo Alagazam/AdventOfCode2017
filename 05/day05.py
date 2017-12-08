3ddca45f59136f20c7aea63d4027ae54a57ca514
David Darj <zid@alagazam.net>
2017-12-08 21:55:09 +0100

    Day 5 step 2

--- 05/day05.py
+++ 05/day05.py
@@ -1,21 +1,39 @@
 # pylint: disable=missing-docstring
+import copy
 
 class Trampolines:
     @staticmethod
     def jumpsteps1(registers):
         counter = 0
-        pc = 0
-        while pc >= 0 and pc < len(registers):
-            next = pc + registers[pc]
-            registers[pc] += 1
-            pc = next
+        index = 0
+        while index >= 0 and index < len(registers):
+            nextindex = index + registers[index]
+            registers[index] += 1
+            index = nextindex
             counter += 1
 
         return counter
 
+    @staticmethod
+    def jumpsteps2(registers):
+        counter = 0
+        index = 0
+        while index >= 0 and index < len(registers):
+            steps = registers[index]
+            nextindex = index + steps
+            if steps >= 3:
+                registers[index] -= 1
+            else:
+                registers[index] += 1
+            index = nextindex
+            counter += 1
+
+        return counter
 
 if __name__ == '__main__':
     FILE = open("input.txt", "r")
     INDATA = [int(x) for x in FILE.read().splitlines()]
     FILE.close()
-    print("Trampolines: " , Trampolines.jumpsteps1(INDATA))
+    INDATA2 = copy.copy(INDATA)
+    print("Trampolines1: ", Trampolines.jumpsteps1(INDATA))
+    print("Trampolines2: ", Trampolines.jumpsteps2(INDATA2))
--- 05/test05.py
+++ 05/test05.py
@@ -4,8 +4,13 @@ import unittest
 from day05 import Trampolines
 
 class Test05(unittest.TestCase):
+
     def test_jumpsteps1(self):
         self.assertEqual(5, Trampolines.jumpsteps1([0, 3, 0, 1, -3]))
+
+    def test_jumpsteps2(self):
+        self.assertEqual(10, Trampolines.jumpsteps2([0, 3, 0, 1, -3]))
+
 
 if __name__ == '__main__':
     unittest.main()
