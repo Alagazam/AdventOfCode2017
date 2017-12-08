c308bd2d6f9d7c016ef5bede4bb10a2b0dd426f7
David Darj <zid@alagazam.net>
2017-12-08 22:49:10 +0100

    Day 6 part 1

---
+++ 06/day06.py
@@ -0,0 +1,43 @@
+# pylint: disable=missing-docstring
+import copy
+
+class Realloc:
+    @staticmethod
+    def maxindex(banks):
+        maxval = banks[0]
+        maxi = 0
+        for index, val in enumerate(banks):
+            if val > maxval:
+                maxval = val
+                maxi = index
+        return maxi
+
+    @staticmethod
+    def redistribute(banks):
+        maxi = Realloc.maxindex(banks)
+        blocks = banks[maxi]
+        banks[maxi] = 0
+        for counter in range(blocks):
+            banks[(maxi+counter+1) % len(banks)] += 1
+        return banks
+
+    @staticmethod
+    def find_loop(banks):
+        distributions = []
+        distributions.append(banks)
+        counter = 0
+        new_distr = copy.copy(banks)
+        while True:
+            counter += 1
+            new_distr = Realloc.redistribute(copy.copy(new_distr))
+            if new_distr in distributions:
+                break
+            distributions.append(new_distr)
+        return counter
+
+if __name__ == '__main__':
+    FILE = open("input.txt", "r")
+    INDATA = [int(x) for x in FILE.read().split("\t")]
+    print(INDATA)
+    FILE.close()
+    print("Reallloc: ", Realloc.find_loop(INDATA))
---
+++ 06/test06.py
@@ -0,0 +1,26 @@
+# pylint: disable=missing-docstring
+
+import unittest
+from day06 import Realloc
+
+class Test05(unittest.TestCase):
+
+    def test_maxindex(self):
+        self.assertEqual(2, Realloc.maxindex([0, 2, 7, 0]))
+        self.assertEqual(1, Realloc.maxindex([2, 4, 1, 2]))
+        self.assertEqual(0, Realloc.maxindex([3, 1, 2, 3]))
+        self.assertEqual(3, Realloc.maxindex([0, 2, 3, 4]))
+
+    def test_redistribute(self):
+        self.assertEqual([2, 4, 1, 2], Realloc.redistribute([0, 2, 7, 0]))
+        self.assertEqual([3, 1, 2, 3], Realloc.redistribute([2, 4, 1, 2]))
+        self.assertEqual([0, 2, 3, 4], Realloc.redistribute([3, 1, 2, 3]))
+        self.assertEqual([1, 3, 4, 1], Realloc.redistribute([0, 2, 3, 4]))
+        self.assertEqual([2, 4, 1, 2], Realloc.redistribute([1, 3, 4, 1]))
+
+    def test_find_loop(self):
+        testdata = [0, 2, 7, 0]
+        self.assertEqual(5, Realloc.find_loop(testdata))
+
+if __name__ == '__main__':
+    unittest.main()
